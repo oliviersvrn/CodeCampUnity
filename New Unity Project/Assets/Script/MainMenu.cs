d414557de4f1f717d5c494ee4badbd6b11da06ed
forestguillaume <forest_g@g03-r4p04.local>
2016-03-24 11:36:07 +0100

    oui

---
+++ New Unity Project/Assets/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+//	public float guiPlacementY;
+//
+//	public float guiPlacementX;
+
+	void OnGUI(){
+
+		///Fait apparaitre les boutons au lancemen
+		if (GUI.Button (new Rect(Screen.width * .3f, Screen.height * .4f, Screen.width * .4f, Screen.height * .1f), "Play")){
+			SceneManager.LoadScene ("Scene test");
+		}
+		if (GUI.Button (new Rect(Screen.width * .3f, Screen.height * .6f, Screen.width * .4f, Screen.height * .1f), "Quit")){
+			Application.Quit();
+		}
+	}
+}
---
+++ New Unity Project/Assets/Script/Menu Pause.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuPause : MonoBehaviour {
+		void OnGUI(){
+			if (GUI.Button (new Rect(Screen.width * .3f, Screen.height * .4f, Screen.width * .4f, Screen.height * .1f), "Main Menu")){
+			SceneManager.LoadScene ("Main Menu");
+		}
+	}
+}
